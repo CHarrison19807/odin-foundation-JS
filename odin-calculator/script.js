a76a46063b5cae0d6250e2cb33ea39931565b4f8
Connor Harrison <connorh.uottawa@gmail.com>
2023-08-16 17:00:42 -0400

    Complete design of calculator, add text to buttons

---
+++ odin-calculator/script.js
@@ -0,0 +1,56 @@
+const zeroButton = document.querySelector("#zero-button");
+zeroButton.textContent = "0";
+
+const oneButton = document.querySelector("#one-button");
+oneButton.textContent = "1";
+
+const twoButton = document.querySelector("#two-button");
+twoButton.textContent = "2";
+
+const threeButton = document.querySelector("#three-button");
+threeButton.textContent = "3";
+
+const fourButton = document.querySelector("#four-button");
+fourButton.textContent = "4";
+
+const fiveButton = document.querySelector("#five-button");
+fiveButton.textContent = "5";
+
+const sixButton = document.querySelector("#six-button");
+sixButton.textContent = "6";
+
+const sevenButton = document.querySelector("#seven-button");
+sevenButton.textContent = "7";
+
+const eightButton = document.querySelector("#eight-button");
+eightButton.textContent = "8";
+
+const nineButton = document.querySelector("#nine-button");
+nineButton.textContent = "9";
+
+const acButton = document.querySelector("#ac-button");
+acButton.textContent = "AC";
+
+const signButton = document.querySelector("#sign-button");
+signButton.textContent = "+/-";
+
+const percentButton = document.querySelector("#percent-button");
+percentButton.textContent = "%";
+
+const divideButton = document.querySelector("#divide-button");
+divideButton.textContent = "/";
+
+const multButton = document.querySelector("#multiply-button");
+multButton.textContent = "x";
+
+const subButton = document.querySelector("#subtract-button");
+subButton.textContent = "-";
+
+const addButton = document.querySelector("#add-button");
+addButton.textContent = "+";
+
+const equalButton = document.querySelector("#equals-button");
+equalButton.textContent = "=";
+
+const decimalButton = document.querySelector("#decimal-button");
+decimalButton.textContent = ".";
